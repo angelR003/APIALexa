3d313bafc785268c636561e1fd85692d60f04346
angelR003 <220432@utxicotepec.edu.mx>
2025-05-22 20:36:23 -0600

    corrección ruta

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-// src/app.js
-
 const express = require('express');
 const app = express();
 const viajesRoutes = require('./routes/viajes_routes');
 
+// Middleware para JSON
 app.use(express.json());
+
+// Redirige la ruta raíz ("/") a "/viajes"
+app.get('/', (req, res) => {
+  res.redirect('/viajes');  // HTTP 302 (redirección temporal)
+});
+
+// Rutas de viajes
 app.use('/viajes', viajesRoutes);
 
 module.exports = app;
